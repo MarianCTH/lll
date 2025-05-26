79527f28359dc7e125b4b5e04974fa06ecbf4ffb
Marian <marianczr7@gmail.com>
2025-05-26 09:18:21 +0300

    a

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 For frontend:
+1. Set up the env:
+   npx create-next-app@latest frontend
 
-Set up the env: npx create-next-app@latest frontend
+2. Install dependencies:
+    cd frontend
+    npm install --legacy-peer-deps
 
-Install dependencies: cd frontend npm install --legacy-peer-deps
-
-Start the development server: npm run dev
+4. Start the development server:
+    npm run dev
--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,18 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
+module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '^react-apexcharts$': '<rootDir>/__mocks__/react-apexcharts.tsx',
   },
-  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  testMatch: ['**/__tests__/**/*.test.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      useESM: true,
-    }],
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(@testing-library/react|@testing-library/jest-dom)/)',
+    '/node_modules/(?!apexcharts|react-apexcharts)/',
   ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 }; 
